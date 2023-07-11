5feaabef9d379a5205664b43fbef24b6646fc1af
JorgeMonteroV <jorgeemv23@gmail.com>
2023-07-11 14:00:51 -0600

    added live tracker

--- hangman/main.py
+++ hangman/main.py
@@ -1,50 +1,103 @@
 #Step 1 
 import random
-word_list = ["aardvark", "baboon", "camel"]
 
-#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+end_of_game = False
+word_list = ["aardvark", "baboon", "camel","onepiece", "ujutsukaisen"]
 chosen_word = random.choice(word_list)
+lives = 6
 
-#Testing code
 print(f'Pssst, the solution is {chosen_word}.')
 
-#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-
-guess = input("Choose a letter:  ").lower()
-
-#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
-#for letter in chosen_word:
-   # if letter == guess:
-      #  print("Right")
-    #else:
-     #   print("Wrong")
-#Step 2
-
-
-
-
-#TODO-1: - Create an empty List called display.
-#For each letter in the chosen_word, add a "_" to 'display'.
-#So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
-
 display = []
-
 word_length = len(chosen_word)
 for letter in chosen_word:
     display.append("_")
-  
+
+    
+while not end_of_game:
+    print(display)
+    print(f"You have {lives} lives.")
+
+    guess = input("Choose a letter:  ").lower()
+
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+        
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
+            print(f"The secret word was {chosen_word}")
+            
+        
+
+    
+    if "_" not in display:
+        end_of_game = True
+        print(display)
+        print("You won!")
+
+    print(stages[lives])
 
 
-#TODO-2: - Loop through each position in the chosen_word;
-#If the letter at that position matches 'guess' then reveal that letter in the display at that position.
-#e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
-
-for position in range(word_length):
-    letter = chosen_word[position]
-    if letter == guess:
-        display[position] = letter
-
-#TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
-#Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
-print(display)
